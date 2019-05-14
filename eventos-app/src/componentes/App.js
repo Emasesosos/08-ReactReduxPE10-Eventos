3bef7d6336cd06e8f4241be56ed08e48264a3f9f
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2019-05-14 00:08:16 -0500

    Guardando categorias, Creando Formulario, Llenando y leyendo Formulario, Consultando API

--- eventos-app/src/componentes/App.js
+++ eventos-app/src/componentes/App.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import Formulario from './Formulario';
 import Imagen from './Imagen';
 import './../css/App.css';
 
 class App extends Component {
+
+  token = 'M5FF6KD4C72VPBKWTY4N';
+  ordenar = 'date';
+
+  state = {
+    categorias: [],
+    eventos: [],
+  }
+
+  componentDidMount() {
+    console.log('Listo');
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias = async () => {
+
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+
+    await fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(categorias => {
+        this.setState({
+          categorias: categorias.categories,
+        })
+      })
+  }
+
+  obtenerEventos = async (busqueda) => {
+    // console.log(busqueda);
+    
+    let url = `https://www.eventbriteapi.com/v3/events/search/?q=${busqueda.nombre}&categories=${busqueda.categoria}&sort_by=${this.ordenar}&token=${this.token}&locale=es_ES`;
+    // console.log(url);
+
+    await fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(eventos => {
+       // console.log(eventos.events);
+       this.setState({
+         eventos: eventos.events,
+       })
+      })
+    
+  }
+
   render() {
     return(
       <div className="App">
@@ -11,6 +60,13 @@ class App extends Component {
         <Header
                 titulo='Eventos'
         ></Header>
+        { /* ***** Componente: Formulario ***** */}
+        <div className="uk-container">
+          <Formulario
+                      categorias={this.state.categorias}
+                      obtenerEventos={this.obtenerEventos}
+          ></Formulario>      
+        </div>
         { /* ***** Componente: Imagen ***** */}
         <Imagen></Imagen>
       </div>
